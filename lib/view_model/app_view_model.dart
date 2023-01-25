521b11a8c705794cda53f0a7138bf2e005bd2aa6
Mac_Developer <araozangel842@gmail.com>
2023-01-25 13:08:41 +0200

    Add default colors

--- lib/view_model/app_view_model.dart
+++ lib/view_model/app_view_model.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_todo/models/task_model.dart';
 
 import '../models/user_model.dart';
@@ -6,4 +6,9 @@ import '../models/user_model.dart';
 class AppViewModel extends ChangeNotifier {
   List<Task> task = <Task>[];
   User user = User('John Doe');
+
+  Color clr1 = Colors.grey.shade50;
+  Color clr2 = Colors.grey.shade200;
+  Color clr3 = Colors.grey.shade800;
+  Color clr4 = Colors.grey.shade900;
 }
